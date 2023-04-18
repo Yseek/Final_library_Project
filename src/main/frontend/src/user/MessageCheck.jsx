3786c2f7ff9cf46c46ae155ca55e1e7ba372ab9c
gorilla2005 <gorilla2005@naver.com>
2023-04-18 13:29:15 +0900

    쪽지 펼치기....

--- src/main/frontend/src/user/MessageCheck.jsx
+++ src/main/frontend/src/user/MessageCheck.jsx
@@ -1,25 +1,25 @@
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import './css/BookList.css';
+import './css/MessageCheck.css';
 
 export default function MessageCheck() {
 
-	const params = useParams();
+  const params = useParams();
 
-	const [data, setData] = useState([]);
-	const [page, setPage] = useState([]);
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState([]);
 
-	useEffect(() => {
-		fetch(`http://127.0.0.1:8080/user/messageCheck?page=${params.page}`)
-			.then(res => res.json())
-			.then(data => { setData(data.content) })
-	}, [params]);
+  useEffect(() => {
+    fetch(`http://127.0.0.1:8080/user/messageCheck?page=${params.page}`)
+      .then(res => res.json())
+      .then(data => { setData(data.content) })
+  }, [params]);
 
-	useEffect(() => {
-		fetch(`http://127.0.0.1:8080/user/messageCheck?page=${params.page}`)
-			.then(res => res.json())
-			.then(page => { setPage(page) })
-	}, [params]);
+  useEffect(() => {
+    fetch(`http://127.0.0.1:8080/user/messageCheck?page=${params.page}`)
+      .then(res => res.json())
+      .then(page => { setPage(page) })
+  }, [params]);
 
   function deleteMessage(messageSeq) {
     if (window.confirm("삭제하시겠습니까?")) {
@@ -34,38 +34,41 @@ export default function MessageCheck() {
     }
   }
 
-	const pageList = Array.from({ length: page.totalPages }, (_, index) => index + 1);
+  //const [visible, setVisible] = useState(false); // 내용접기, 펼치기
 
-	return (
-		<div className='BookListDiv'>
-			<button><Link to={`/`}>메인메인!!!!</Link></button>
-			<table className='BookListTable'>
-				<thead>
-					<tr>
-						<th className='BookListTh'>번호</th>
-						<th className='BookListTh'>날짜</th>
-						<th className='BookListTh'>내용</th>
-            <th className='BookListTh'>삭제</th>
-					</tr>
-				</thead>
-				<tbody>
-					{Array.isArray(data) && data.map(res => (
-						<tr key={res.messageSeq}>
-							<td className='BookListTd'>{res.messageSeq}</td>
-							<td className='BookListTd'>{res.messageDate}</td>
-							<td className='BookListTd'>{res.messageContent}</td>
-              <td className='BookListTd'><button onClick={() => deleteMessage(res.messageSeq)}>삭제</button></td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-			<div className="page">
-				{pageList.map(res => (
-					<span key={res}>
-						<Link to={`/user/MessageCheck/${res}`}>{res}</Link>
-					</span>
-				))}
-			</div>
-		</div>
-	);
+  const pageList = Array.from({ length: page.totalPages }, (_, index) => index + 1);
+
+  return (
+    <div className='MsgDiv'>
+      <button><Link to={`/`}>메인메인!!!!</Link></button>
+      <table className='MsgTable'>
+        <thead>
+          <tr>
+            <th className='MsgTh'>번호</th>
+            <th className='MsgTh'>날짜</th>
+            <th className='MsgTh'>내용 보기</th>
+            <th className='MsgTh'>삭제</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.isArray(data) && data.map(res => (
+            <tr key={res.messageSeq}>
+              <td className='MsgTd'>{res.messageSeq}</td>
+              <td className='MsgTd'>{res.messageDate}</td>
+              {/* <td className='MsgTd'><button className='MsgBtn' onClick={() => { setVisible(!visible);}}>{visible ? "접기" : "내용보기"}</button><p>{visible && res.messageContent}</p></td> */}
+              <td className='MsgTd'>{res.messageContent}</td>
+              <td className='MsgTd'><button className='MsgBtn' onClick={() => deleteMessage(res.messageSeq)}>삭제</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="page">
+        {pageList.map(res => (
+          <span key={res}>
+            <Link to={`/user/MessageCheck/${res}`}>{res}</Link>
+          </span>
+        ))}
+      </div>
+    </div>
+  );
 }
