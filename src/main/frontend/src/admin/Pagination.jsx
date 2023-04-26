0c7c1daaef314b794443a0683d87c9701a7cc497
pks341 <wlwhzld1212@naver.com>
2023-04-26 16:05:00 +0900

    ㅡmerge standby

--- src/main/frontend/src/admin/Pagination.jsx
+++ src/main/frontend/src/admin/Pagination.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 function Pagination({ total, limit, page, setPage}){
     const numPages = Math.ceil(total/limit);
-
+    const startPages = Math.ceil(page/limit);
     return(
         <div>
             <Button onClick={() => setPage(page-1)} disabled={page === 1}>
