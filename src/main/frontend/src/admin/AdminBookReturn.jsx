a84feb632fc7f820d246bfe629eaa1ea7277fd76
gorilla2005 <gorilla2005@naver.com>
2023-05-02 10:41:57 +0900

    a

--- src/main/frontend/src/admin/AdminBookReturn.jsx
+++ src/main/frontend/src/admin/AdminBookReturn.jsx
@@ -3,53 +3,54 @@ import Ip from "../Ip";
 import Pagination from "./Pagination";
 import moment from 'moment';
 import styled from "styled-components";
+import './css/AdminBookReturn.css';
 
-export default function AdminBookReturn(){
+export default function AdminBookReturn() {
     const [rentList, setrentList] = useState([])
     const [isListAll, setisListAll] = useState(true)
     const [limit, setLimit] = useState(5);
     const [page, setPage] = useState(1);
     const offset = (page - 1) * limit;
-    
+
     useEffect(() => {
-        if(isListAll){
+        if (isListAll) {
             listAll();
-        }else if(!isListAll){
+        } else if (!isListAll) {
             searchKeyword();
         }
     }, [])
 
-    function listAll(){
+    function listAll() {
         fetch(`${Ip.url}/admin/return`, {
             method: "GET",
-            headers:{
+            headers: {
                 "Content-Type": "application/json",
                 "Authorization": "Bearer " + sessionStorage.getItem("token"),
             },
         })
-        .then(res => res.json())
-        .then(data => setrentList(data))
-        .catch(error => console.error(error));
+            .then(res => res.json())
+            .then(data => setrentList(data))
+            .catch(error => console.error(error));
     };
 
-    function optionCheck(value){
-        switch(value){
+    function optionCheck(value) {
+        switch (value) {
             case "bookReserveSeq": setisListAll(false); break;
             case "bookSeq": setisListAll(false); break;
-            default: {setisListAll(true); setLimit(Number(10));}
+            default: { setisListAll(true); setLimit(Number(10)); }
         }
     }
 
-    function searchKeyword(e){
+    function searchKeyword(e) {
         e.preventDefault();
 
         let option = e.target.option.value;
-        if(option.length<1){
+        if (option.length < 1) {
             alert("검색어를 입력해주세요")
             return;
         }
         let keyWord = e.target.keyWord.value;
-        if(keyWord.length<1){
+        if (keyWord.length < 1) {
             alert("검색어를 입력해주세요")
             return;
         }
@@ -62,15 +63,15 @@ export default function AdminBookReturn(){
                 "Authorization": "Bearer " + sessionStorage.getItem("token"),
             },
         })
-        .then(res => res.json())
-        .then(data => setrentList(data))
-        .catch(error => console.log(error));
+            .then(res => res.json())
+            .then(data => setrentList(data))
+            .catch(error => console.log(error));
     }
 
-    function returnBook(e, bookRentSeq, bookSeq){
+    function returnBook(e, bookRentSeq, bookSeq) {
         e.preventDefault();
 
-        if(window.confirm(`${bookSeq}번 도서를 반납처리합니다.`)){
+        if (window.confirm(`${bookSeq}번 도서를 반납처리합니다.`)) {
             fetch(`${Ip.url}/admin/return/${bookRentSeq}&${bookSeq}`, {
                 method: "POST",
                 headers: {
@@ -78,51 +79,52 @@ export default function AdminBookReturn(){
                     Authorization: "Bearer " + sessionStorage.getItem("token"),
                 },
             })
-            .then(() => {
-                alert("처리가 완료되었습니다.");
-                window.location.reload();
-            })
+                .then(() => {
+                    alert("처리가 완료되었습니다.");
+                    window.location.reload();
+                })
         }
     }
 
-    return(
-    <>
-        <div>
-            <Table>
+    return (
+        <div className='adminBookReturnDiv'>
+            <h2>도서반납 처리</h2>
+            <table className='adminBookReturnTable'>
                 <thead>
                     <tr>
-                        <td>대출번호</td>
-                        <td>대출자</td>
-                        <td>책번호</td>
-                        <td>책제목</td>
-                        <td>대출일</td>
-                        <td>반납기한</td>
+                        <th className='adminBookReturnTableTh'>대출번호</th>
+                        <th className='adminBookReturnTableTh'>대출자</th>
+                        <th className='adminBookReturnTableTh'>책번호</th>
+                        <th className='adminBookReturnTableTh'>책제목</th>
+                        <th className='adminBookReturnTableTh'>대출일</th>
+                        <th className='adminBookReturnTableTh'>반납기한</th>
+                        <th className='adminBookReturnTableTh'>반납처리</th>
                     </tr>
                 </thead>
                 <tbody>
-                {rentList.slice(offset, offset + limit).map((rnt, index) => (
+                    {rentList.slice(offset, offset + limit).map((rnt, index) => (
                         <tr key={index}>
-                        <td>{rnt.bookRentSeq}</td>
-                        <td>{rnt.memberName}</td>
-                        <td>{rnt.bookSeq}</td>
-                        <td>{rnt.bookTitle}</td>
-                        <td>{moment(rnt.bookRentRDate).format('YYYY-MM-DD HH:mm:ss')}</td>
-                        <td>{moment(rnt.bookRentDDay).format('YYYY-MM-DD HH:mm:ss')}</td>
-                        <td>
-                            <button onClick={(e) => returnBook(e, rnt.bookRentSeq, rnt.bookSeq)}>반납</button>
-                        </td>
-                    </tr>
-                ))}
-                </tbody>      
-            </Table>
-            <div>
+                            <td className='adminBookReturnTableTd'>{rnt.bookRentSeq}</td>
+                            <td className='adminBookReturnTableTd'>{rnt.memberName}</td>
+                            <td className='adminBookReturnTableTd'>{rnt.bookSeq}</td>
+                            <td className='adminBookReturnTableTd'>{rnt.bookTitle}</td>
+                            <td className='adminBookReturnTableTd'>{moment(rnt.bookRentRDate).format('YYYY-MM-DD HH:mm:ss')}</td>
+                            <td className='adminBookReturnTableTd'>{moment(rnt.bookRentDDay).format('YYYY-MM-DD HH:mm:ss')}</td>
+                            <td className='adminBookReturnTableTd'>
+                                <button className='adminBookReturnBtn' onClick={(e) => returnBook(e, rnt.bookRentSeq, rnt.bookSeq)}>반납</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <div className='adminBookRentSearchDiv'>
                 <form name="e" autoComplete="off" onSubmit={searchKeyword}>
-                    <select name="option" onChange={({ target: { value } }) => optionCheck(value)}>
+                    <select className='adminBookRentSearchSelect' name="option" onChange={({ target: { value } }) => optionCheck(value)}>
                         <option value="bookRentSeq">대출번호</option>
                         <option value="bookSeq">책번호</option>
                     </select>
-                    <input type="text" name="keyWord" placeholder=""></input>
-                    <button>검색</button>
+                    <input className='adminBookRentSearchInput' type="text" name="keyWord" placeholder="검색어를 입력해주세요"></input>
+                    <button className='adminBookReturnSearchBtn'>검색</button>
                 </form>
             </div>
             <span>
@@ -134,7 +136,6 @@ export default function AdminBookReturn(){
                 />
             </span>
         </div>
-    </>
     )
 }
 
